3ce8dc4f6e80eaf87a81029b3fc221ea578643f5
SaraClements <saraclements1225@gmail.com>
2020-02-11 11:13:41 -0800

    configure sqlalchemy

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import os
 
+#set base directory path as variable
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'fuckoff'
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'fuck-off'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+		'sqlite:///' + os.path.join(basedir, 'app.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
